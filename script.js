cbd3e72cff066e1cca6996b988d6c1ee03239149
juliuskamau <kamaujulius077@gmail.com>
2026-05-15 09:33:09 +0300

    chore(js):set up storage

---
+++ script.js
@@ -0,0 +1,4 @@
+const placeForm = document.getElementById('placeForm');
+const placesList = document.getElementById('placesList');
+const emptyState = document.getElementById('empty-state');
+const successMessage = document.getElementById('success-message');
